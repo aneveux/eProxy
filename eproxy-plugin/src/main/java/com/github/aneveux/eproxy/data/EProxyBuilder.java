bc70f7633caa9f3b803e4e9e315631eb0ebe9d38
Antoine Neveux <antoine.neveux@gmail.com>
2012-10-22 17:29:14 +0200

    Added fluent API on top of EProxy object
    Signed-off-by: Antoine Neveux <antoine.neveux@gmail.com>

---
+++ eproxy-plugin/src/main/java/com/github/aneveux/eproxy/data/EProxyBuilder.java
@@ -0,0 +1,58 @@
+package com.github.aneveux.eproxy.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EProxyBuilder {
+
+	private EProxy result;
+
+	private List<String> nonProxyHosts;
+
+	public EProxyBuilder() {
+		this.result = new EProxy();
+		this.nonProxyHosts = new ArrayList<String>();
+	}
+
+	public EProxyBuilder withReference(String reference) {
+		this.result.setReference(reference);
+		return this;
+	}
+
+	public EProxyBuilder withHost(String host) {
+		this.result.setHost(host);
+		return this;
+	}
+
+	public EProxyBuilder withPort(int port) {
+		this.result.setPort(port);
+		return this;
+	}
+
+	public EProxyBuilder withAuthenticationRequired(
+			boolean authenticationRequired) {
+		this.result.setAuthenticationRequired(authenticationRequired);
+		return this;
+	}
+
+	public EProxyBuilder withUser(String user) {
+		this.result.setUser(user);
+		return this;
+	}
+
+	public EProxyBuilder withPassword(String password) {
+		this.result.setPassword(password);
+		return this;
+	}
+
+	public EProxyBuilder withNonProxyHost(String nonProxyHost) {
+		this.nonProxyHosts.add(nonProxyHost);
+		return this;
+	}
+
+	public EProxy build() {
+		this.result.setNonProxyHosts(this.nonProxyHosts);
+		return this.result;
+	}
+
+}
